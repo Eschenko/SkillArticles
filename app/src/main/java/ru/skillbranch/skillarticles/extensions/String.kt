cbe3bd033414d50310bb1cf3ed4b7c382fea408b
Eschenko <valhom12@yandex.ru>
2021-07-22 16:21:00 +0500

    initial commit

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -6,7 +6,7 @@ fun String?.indexesOf(
 ): List<Int> {
     var index = 0
     val set = mutableSetOf<Int>()
-    if (this != null) {
+    if (this != null && substr !="") {
         for (i in 0..this.length) {
             index = this.indexOf(substr, index + 1, ignoreCase)
             set.add(index)
