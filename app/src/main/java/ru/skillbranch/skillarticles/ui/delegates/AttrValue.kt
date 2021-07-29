82f921a2ebe5dd8d3e222919efdedf2ece0cb034
Eschenko <valhom12@yandex.ru>
2021-07-29 18:17:13 +0500

    initial commit

--- app/src/main/java/ru/skillbranch/skillarticles/ui/delegates/AttrValue.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/delegates/AttrValue.kt
@@ -5,10 +5,9 @@ import android.content.res.Resources
 import android.util.TypedValue
 import androidx.annotation.AttrRes
 import kotlin.properties.ReadOnlyProperty
-import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class AttrValue(@AttrRes private var res: Int) : ReadWriteProperty<Context, Int> {
+class AttrValue(@AttrRes private val res: Int) : ReadOnlyProperty<Context, Int>{
     private var _value : Int? = null
     override fun getValue(thisRef: Context, property: KProperty<*>): Int {
         if (_value == null){
@@ -16,12 +15,7 @@ class AttrValue(@AttrRes private var res: Int) : ReadWriteProperty<Context, Int>
             if (thisRef.theme.resolveAttribute(res, tv, true)) _value = tv.data
             else throw Resources.NotFoundException("Resource with id $res not found")
         }
-        return _value!!
-    }
-
-    override fun setValue(thisRef: Context, property: KProperty<*>, value: Int) {
-        this.res = value
-
+        return res
     }
 
 }
