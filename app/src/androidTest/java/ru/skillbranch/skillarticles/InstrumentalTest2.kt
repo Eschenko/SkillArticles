ad74521b294f69e30efb3211c54cd98d1c5e412c
Eschenko <valhom12@yandex.ru>
2021-08-07 16:01:01 +0500

    initial commit

--- app/src/androidTest/java/ru/skillbranch/skillarticles/InstrumentalTest2.kt
+++ app/src/androidTest/java/ru/skillbranch/skillarticles/InstrumentalTest2.kt
@@ -1,5 +1,5 @@
 package ru.skillbranch.skillarticles
-
+//
 //import androidx.test.core.app.ActivityScenario
 //import androidx.test.ext.junit.runners.AndroidJUnit4
 //import androidx.test.platform.app.InstrumentationRegistry
